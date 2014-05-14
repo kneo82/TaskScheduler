f1f1f821773a0516313b140facf3dd3e99a45c11
kneo82 <kneo82@gmail.com>
2014-05-14 08:59:18 +0300

    no message

---
+++ TaskScheduler/Controller/CalendarViewController/TSCalendarView.m
@@ -0,0 +1,46 @@
+//
+//  TSCalendarView.m
+//  TaskScheduler
+//
+//  Created by Vitaliy Voronok on 5/13/14.
+//  Copyright (c) 2014 Vitaliy Voronok. All rights reserved.
+//
+
+#import "TSCalendarView.h"
+#import "CKCalendarView.h"
+
+@implementation TSCalendarView
+
+#pragma mark -
+#pragma mark Initializations and Deallocations
+
+- (void)dealloc {
+    self.calendar = nil;
+    
+    [super dealloc];
+}
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+    }
+    return self;
+}
+
+#pragma mark -
+#pragma mark View Lifecycle
+
+- (void)awakeFromNib {
+    CKCalendarView *calendar = self.calendar;
+    
+    calendar.onlyShowCurrentMonth = NO;
+    calendar.adaptHeightToNumberOfWeeksInMonth = YES;
+    [calendar setMonthButtonColor:[UIColor grayColor]];
+    [calendar setInnerBorderColor:[UIColor blueColor]];
+    [calendar setDayOfWeekBottomColor:[UIColor blackColor] topColor:[UIColor darkGrayColor]];
+    calendar.dayOfWeekTextColor = [UIColor whiteColor];
+    calendar.backgroundColor = [UIColor blackColor];
+}
+
+@end
---
+++ TaskScheduler/Controller/CalendarViewController/TSCalendarViewController.m
@@ -0,0 +1,111 @@
+//
+//  TSCalendarViewController.m
+//  TaskScheduler
+//
+//  Created by Vitaliy Voronok on 5/13/14.
+//  Copyright (c) 2014 Vitaliy Voronok. All rights reserved.
+//
+
+#import "TSCalendarViewController.h"
+#import "TSCalendarView.h"
+#import "CKCalendarView.h"
+
+#import "UIViewController+IDPExtensions.h"
+
+@interface TSCalendarViewController () <CKCalendarDelegate>
+@property (nonatomic, readonly) TSCalendarView *calendarView;
+@property(nonatomic, retain) UILabel *dateLabel;
+@property(nonatomic, retain) NSDateFormatter *dateFormatter;
+@property(nonatomic, retain) NSDate *minimumDate;
+@property(nonatomic, retain) NSArray *disabledDates;
+
+@end
+
+@implementation TSCalendarViewController
+
+#pragma mark -
+#pragma mark Initializations and Deallocations
+
+- (void)dealloc {
+    self.dateLabel = nil;
+    self.dateFormatter = nil;
+    self.minimumDate = nil;
+    self.disabledDates = nil;
+    
+    [super dealloc];
+}
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+
+    }
+    return self;
+}
+
+#pragma mark -
+#pragma mark View Lifecycle
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+//    self.calendarView.calendarView.de
+}
+
+- (void)localeDidChange {
+    [self.calendar setLocale:[NSLocale currentLocale]];
+}
+
+#pragma mark -
+#pragma mark Accessors
+
+IDPViewControllerViewOfClassGetterSynthesize(TSCalendarView, calendarView);
+
+#pragma mark -
+#pragma mark Private
+
+- (BOOL)dateIsDisabled:(NSDate *)date {
+    for (NSDate *disabledDate in self.disabledDates) {
+        if ([disabledDate isEqualToDate:date]) {
+            return YES;
+        }
+    }
+    return NO;
+}
+
+#pragma mark -
+#pragma mark - CKCalendarDelegate
+
+- (void)calendar:(CKCalendarView *)calendar configureDateItem:(CKDateItem *)dateItem forDate:(NSDate *)date {
+    // TODO: play with the coloring if we want to...
+//    NSLog(@"Configure %@", date);
+    if ([self dateIsDisabled:date]) {
+        dateItem.backgroundColor = [UIColor redColor];
+        dateItem.textColor = [UIColor whiteColor];
+    }
+}
+
+- (BOOL)calendar:(CKCalendarView *)calendar willSelectDate:(NSDate *)date {
+    return ![self dateIsDisabled:date];
+}
+
+- (void)calendar:(CKCalendarView *)calendar didSelectDate:(NSDate *)date {
+    self.dateLabel.text = [self.dateFormatter stringFromDate:date];
+}
+
+- (BOOL)calendar:(CKCalendarView *)calendar willChangeToMonth:(NSDate *)date {
+    if ([date laterDate:self.minimumDate] == date) {
+        self.calendar.backgroundColor = [UIColor blueColor];
+        return YES;
+    } else {
+        self.calendar.backgroundColor = [UIColor redColor];
+        return NO;
+    }
+}
+
+- (void)calendar:(CKCalendarView *)calendar didLayoutInRect:(CGRect)frame {
+    //    NSLog(@"calendar layout: %@", NSStringFromCGRect(frame));
+}
+
+@end
